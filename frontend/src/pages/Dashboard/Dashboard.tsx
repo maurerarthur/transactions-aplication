688361a012d5c62b8f4acfe5d88715c8d8c5d951
Arthur Maurer <30410972+maurerarthur@users.noreply.github.com>
2024-02-17 17:36:24 -0300

    Update Dashboard.tsx

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
-import { LineChart } from 'react-chartkick'
-import 'chartkick/chart.js'
+// import { LineChart } from 'react-chartkick'
+// import 'chartkick/chart.js'
 
 import { useLoginStore } from '../Login/store'
 import { fetchTransactionsResume } from './services'
@@ -15,16 +15,16 @@ const Dashboard: React.FC = () => {
     queryFn: () => fetchTransactionsResume(id)
   })
 
-  const resume = transactionsResume.data?.reduce((a, transaction) => ({
-    ...a, [transaction.dueDate]: transaction.type == 'received' ? +transaction.amount : -Math.abs(transaction.amount)
-  }), {})
+  // const resume = transactionsResume.data?.reduce((a, transaction) => ({
+  //   ...a, [transaction.dueDate]: transaction.type == 'received' ? +transaction.amount : -Math.abs(transaction.amount)
+  // }), {})
 
   return(
     <div className='p-2'>
       <h1 className='text-white text-4xl font-bold'>Dashboard</h1>
       <h1 className='text-white text-2xl font-bold'>Your last 10 transactions</h1>
       <div className='bg-white rounded-lg mt-5 p-5'>
-        <LineChart data={resume} />
+      {/* <LineChart data={resume} /> */}
       </div>
     </div>
   )
